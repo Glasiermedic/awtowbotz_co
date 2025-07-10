54da74f3ac88931f7d0e7d679fe346b173a2dd8c
Glasiermedic <wrolfson@glasierstudios.com>
2025-07-10 14:43:41 -0700

    trying to force vercel to update the build

--- frontend/awtowbotz-ui/src/App.js
+++ frontend/awtowbotz-ui/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import SummaryCards from './components/SummaryCards';
+import SummaryCards from './components/SummaryCards'; //dependent on the config.js and .env working on fixing the issues
 import RegionChart from './components/RegionChart';
 import TopRepsList from './components/TopRepsList';
 
 function App() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
-      <h1>aw-tow-botz-co: Transformer Sales Dashboard</h1>
+      <h1>aw-tow-botz-co: Transformer Co Sales Dashboard</h1>
       <SummaryCards />
       <RegionChart />
       <TopRepsList />
